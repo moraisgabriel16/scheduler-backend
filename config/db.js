54ad20e657a2f5e73b8d09e39cc448291b383adf
Gabriel <moraisgabriel16@gmail.com>
2024-11-18 06:49:53 -0300

    ajus db

--- config/db.js
+++ config/db.js
@@ -1,15 +1,16 @@
-// config/db.js
 const mongoose = require('mongoose');
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB conectado...');
+    console.log(`MongoDB conectado: ${conn.connection.host}`);
   } catch (error) {
-    console.error('Erro ao conectar ao MongoDB:', error);
-    process.exit(1);
+    console.error(`Erro ao conectar ao MongoDB: ${error.message}`);
+    process.exit(1); // Encerra o processo se não conectar
   }
 };
+
 module.exports = connectDB;
