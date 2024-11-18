b0efed8b3f5a5c1bdfe43ebad3189f8eab3bc0ab
Gabriel <moraisgabriel16@gmail.com>
2024-11-18 07:03:01 -0300

    ajuste DB

--- config/db.js
+++ config/db.js
@@ -2,10 +2,15 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      throw new Error('A variável de ambiente MONGO_URI não foi definida.');
+    }
+
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
     console.log(`MongoDB conectado: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Erro ao conectar ao MongoDB: ${error.message}`);
